c195bc9f6b0a63a725e8d2cc0327e11baf0d9ecc
dlwng <dlw4dc@virginia.edu>
2017-09-17 17:18:43 -0400

    some coding view n things

--- clothsie_app/models.py
+++ clothsie_app/models.py
@@ -5,17 +5,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-	items_bought = models.ForeignKey(Items, on_delete=models.CASCADE)
-	items_selling = models.ForeignKey(Items, on_delete=models.CASCADE) 
-	items_sold = models.ForeignKey(Items, on_delete=models.CASCADE)
-
-class Items(models.Model):
+class Item(models.Model):
 	comments = models.TextField()
 	date_added = models.DateField()
 	date_purchased = models.DateField()
@@ -26,3 +16,23 @@ class Items(models.Model):
 	color = models.CharField(max_length=20)
 	brand = models.CharField(max_length=50)
 	description = models.TextField()
+	item_id = models.IntegerField()
+
+	# def detail(item_id):
+	# 	return (comments + date_added + date_purchased + zipcode + price + size 
+	# 		+ category + color + brand + description)
+
+#def delete(item_id):
+
+#	return("Item Deleted")
+
+class User(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	username = models.CharField(max_length=50)
+	password = models.CharField(max_length=50)
+	email = models.EmailField(max_length=100)
+	items_bought = models.ForeignKey(Item, related_name='items_bought', on_delete=models.CASCADE)
+	items_selling = models.ForeignKey(Item, related_name='items_selling', on_delete=models.CASCADE)
+	items_sold = models.ForeignKey(Item, related_name='items_sold', on_delete=models.CASCADE)
+#def delete_item(item_id, username):
--- clothsie_app/views.py
+++ clothsie_app/views.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import User, Item
+
 # Create your views here.
+
+# def detail(request, item_id):
+# 	return HttpResponse(Item.detail(item_id))
+
+#def delete_item(request, item_id):
+#	return HttpResponse(Items.delete(item_id))
