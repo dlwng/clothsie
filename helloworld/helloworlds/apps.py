ebd6f7eaf28d41677584b3a95d6a3f1fe381c617
Vamshi <vkg5xt@virginia.edu>
2017-09-21 19:01:08 -0400

    finalized CRUD operations and APIs

--- helloworld/helloworlds/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HelloworldsConfig(AppConfig):
-    name = 'helloworlds'
--- helloworld/helloworlds/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from . import home
-
-urlpatterns = [
-    url(r'^$', home.index, name='index'),
-]
